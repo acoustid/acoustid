4277f932f2dbe8688142c61ea6b9080dc7b77190
Lukáš Lalinský <lalinsky@gmail.com>
2020-03-03 07:52:04 +0100

    Add track-related created/updated indexes

---
+++ database/sql/fingerprint/migrations/2020_03_03_more_created_updated_indexes.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+CREATE INDEX CONCURRENTLY fingerprint_idx_updated ON fingerprint (updated) WHERE updated IS NOT NULL;
+
+CREATE INDEX CONCURRENTLY track_idx_created ON track (created);
+CREATE INDEX CONCURRENTLY track_idx_updated ON track (updated) WHERE updated IS NOT NULL;
+
+CREATE INDEX CONCURRENTLY track_puid_idx_created ON track_puid (created);
+CREATE INDEX CONCURRENTLY track_puid_idx_updated ON track_puid (updated) WHERE updated IS NOT NULL;
+
+CREATE INDEX CONCURRENTLY track_mbid_idx_created ON track_mbid (created);
+CREATE INDEX CONCURRENTLY track_mbid_idx_updated ON track_mbid (updated) WHERE updated IS NOT NULL;
+
+COMMIT;
--- database/sql/fingerprint/schema.sql
+++ database/sql/fingerprint/schema.sql
@@ -568,4 +568,13 @@ ALTER TABLE ONLY public.track_puid
     ADD CONSTRAINT track_puid_fk_track_id FOREIGN KEY (track_id) REFERENCES public.track(id);
 
 
+CREATE INDEX fingerprint_idx_updated ON fingerprint (updated) WHERE updated IS NOT NULL;
 
+CREATE INDEX track_idx_created ON track (created);
+CREATE INDEX track_idx_updated ON track (updated) WHERE updated IS NOT NULL;
+
+CREATE INDEX track_puid_idx_created ON track_puid (created);
+CREATE INDEX track_puid_idx_updated ON track_puid (updated) WHERE updated IS NOT NULL;
+
+CREATE INDEX track_mbid_idx_created ON track_mbid (created);
+CREATE INDEX track_mbid_idx_updated ON track_mbid (updated) WHERE updated IS NOT NULL;
